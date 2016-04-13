3b48e940407e3c363bc529cf6dce2a26908d76a9
sunjigang <sunjigang@fangdd.com>
2016-04-13 11:39:11 +0800

    子库初始化更新脚本＋添加config.h

---
+++ automatic.sh
@@ -0,0 +1,13 @@
+echo "项目根目录下config.h配置APPID和HASH，文件内有说明"
+
+echo "本项目使用了submodule进行管理接下来更新子项目"
+echo "由于未知原因，子项目的SSH方式访问失败，所以更改项目访问方式为HTTPS"
+
+echo "更新子项目..."
+
+git submodule init
+git submodule update --checkout
+git submodule update
+
+echo "更新完毕。"
+
---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//  需要配合服务器设置的
+#define SETUP_API_ID(apiId) apiId = 1;
+#define SETUP_API_HASH(apiHash) apiHash = @"x";
+//  @albtsantos https://github.com/peter-iakovlev/Telegram/issues/34
+
+#endif
